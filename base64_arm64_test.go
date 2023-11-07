16310029230eeafc6f4d1483e2fb90b1f08c1b8d
Sun Yimin <emmansun@users.noreply.github.com>
2023-11-07 10:41:46 +0800

    test arm move conditial

--- base64_arm64_test.go
+++ base64_arm64_test.go
@@ -90,3 +90,17 @@ func TestUrlDecodeSIMD(t *testing.T) {
 		}
 	}
 }
+
+func TestMoveCond(t *testing.T) {
+	a := []byte{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+	b := []byte{15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0}
+	c := make([]byte, 16)
+	moveCond(&c[0], &a[0], &b[0], 0)
+	if (!bytes.Equal(c, b)) {
+		t.Errorf("expected %x, got %x", b, c)
+	}
+	moveCond(&c[0], &a[0], &b[0], 1)
+	if (!bytes.Equal(c, a)) {
+		t.Errorf("expected %x, got %x", a, c)
+	}
+}
