2ef931c315c63b18d1f119fdaf0c9ccd3c56581a
Sun Yimin <emmansun@users.noreply.github.com>
2025-11-12 11:41:19 +0800

    loong64 decodeStdAsm

--- base64_loong64.s
+++ base64_loong64.s
@@ -27,6 +27,30 @@ DATA base64_const<>+0x60(SB)/8, $0x1919191919191919 // range 0 end
 DATA base64_const<>+0x68(SB)/8, $0x1919191919191919 // range 0 end
 GLOBL base64_const<>(SB), (NOPTR+RODATA), $112
 
+DATA decode_const<>+0x00(SB)/8, $0x0804080402011010 // standard decode lut hi
+DATA decode_const<>+0x08(SB)/8, $0x1010101010101010
+DATA decode_const<>+0x10(SB)/8, $0x1111111111111115 // standard decode lut lo
+DATA decode_const<>+0x18(SB)/8, $0x1A1B1B1B1A131111
+DATA decode_const<>+0x20(SB)/8, $0x2F2F2F2F2F2F2F2F // standard decode mask
+DATA decode_const<>+0x28(SB)/8, $0x2F2F2F2F2F2F2F2F
+DATA decode_const<>+0x30(SB)/8, $0xB9B9BFBF04131000 // standard decode lut roll
+DATA decode_const<>+0x38(SB)/8, $0x0000000000000000
+DATA decode_const<>+0x40(SB)/8, $0x2804080402011010 // url decode lut hi
+DATA decode_const<>+0x48(SB)/8, $0x1010101010101010
+DATA decode_const<>+0x50(SB)/8, $0x1111111111111115 // url decode lut lo
+DATA decode_const<>+0x58(SB)/8, $0x331B1A1B1B131111
+DATA decode_const<>+0x60(SB)/8, $0x5E5E5E5E5E5E5E5E // url decode mask
+DATA decode_const<>+0x68(SB)/8, $0x5E5E5E5E5E5E5E5E
+DATA decode_const<>+0x70(SB)/8, $0xB9E0BFBF04110000 // url decode lut roll
+DATA decode_const<>+0x78(SB)/8, $0x00000000000000B9
+DATA decode_const<>+0x80(SB)/8, $0x0140014001400140 // decode reshufling constant 0
+DATA decode_const<>+0x88(SB)/8, $0x0140014001400140
+DATA decode_const<>+0x90(SB)/8, $0x0001100000011000 // decode reshufling constant 1
+DATA decode_const<>+0x98(SB)/8, $0x0001100000011000
+DATA decode_const<>+0xA0(SB)/8, $0x090A040506000102 // decode reshufling mask
+DATA decode_const<>+0xA8(SB)/8, $0xFFFFFFFF0C0D0E08
+GLOBL decode_const<>(SB), (NOPTR+RODATA), $176
+
 #define RESHUFFLE_MASK V0
 #define SHIFT_RIGHT_MASK V1
 #define MULHI_MASK V2
@@ -65,8 +89,8 @@ loop:
 		VSLLH SHIFT_LEFT_MASK, V8, V8
 		VORV V9, V8, V8
 
-		WORD $0x704c1509              // VSSUBBU RANGE1_END, V8, V9
-		WORD $0x700820ca              // VSLTBU V8, RANGE0_END, V10
+		WORD $0x704c1509             // VSSUBBU RANGE1_END, V8, V9
+		WORD $0x700820ca             // VSLTBU V8, RANGE0_END, V10
 		VSUBB V10, V9, V9
 		WORD $0xd549ce9              // VSHUFB V9, LUT, LUT, V9
 		VADDB V9, V8, V8
@@ -82,8 +106,74 @@ done:
 	MOVV R5, ret+56(FP)
 	RET
 
+#undef RESHUFFLE_MASK
+#undef SHIFT_RIGHT_MASK
+#undef MULHI_MASK
+#undef SHIFT_LEFT_MASK
+#undef MULLO_MASK
+#undef RANGE1_END
+#undef RANGE0_END
+#undef LUT
+
+#define NIBBLE_MASK V0
+#define LUT_HI V1
+#define LUT_LO V2
+#define DECODE_END V3
+#define LUT_ROLL V4
+#define RESHUFFLE_CONST0 V5
+#define RESHUFFLE_CONST1 V6
+#define RESHUFFLE_MASK V7
+
 //func decodeStdAsm(dst, src []byte) int
 TEXT ·decodeStdAsm(SB),NOSPLIT,$0
+	MOVV dst_base+0(FP), R5
+	MOVV src_base+24(FP), R6
+	MOVV src_len+32(FP), R7
+
+	MOVV $decode_const<>(SB), R8
+	VMOVQ (0*16)(R8), LUT_HI
+	VMOVQ (1*16)(R8), LUT_LO
+	VMOVQ (2*16)(R8), DECODE_END
+	VMOVQ (3*16)(R8), LUT_ROLL
+	VMOVQ (8*16)(R8), RESHUFFLE_CONST0
+	VMOVQ (9*16)(R8), RESHUFFLE_CONST1
+	VMOVQ (10*16)(R8), RESHUFFLE_MASK
+	MOVV $24, R10
+loop:
+		VMOVQ (R6), V8        // load 16 bytes input
+		// validate the input data
+		VSRLB $4, V8, V9      // high nibble
+		VANDB $0xf, V8, V10   // low nibble
+		WORD $0xd548429       // VSHUFB V9, LUT_HI, LUT_HI, V9
+		WORD $0xd55084a       // VSHUFB V10, LUT_LO, LUT_LO, V10
+		VANDV V9, V10, V10
+		VSETEQV V10, FCC0
+		BFPF done
+
+		// translate
+		VSEQB V8, DECODE_END, V10 // compare 0x2F with input bytes
+		VADDB V9, V10, V10        // add eq_2F with hi_nibbles
+		WORD $0xd55108a           // VSHUFB V10, LUT_ROLL, LUT_ROLL, V10
+		VADDB V10, V8, V8         // Now simply add the delta values to the input
+
+		// reshuffle output bytes
+		VMULWEVHBU V8, RESHUFFLE_CONST0, V9        // We alos can use vmaddwev.h.bu and vmaddwod.h.bu, then we just need two instructions
+		VMULWODHBU V8, RESHUFFLE_CONST0, V10
+		VADDHU V9, V10, V8
+
+		VMULWEVWHU V8, RESHUFFLE_CONST1, V9
+		VMULWODWHU V8, RESHUFFLE_CONST1, V10
+		VADDWU V9, V10, V8
+
+		WORD $0xd53a108           // VSHUFB RESHUFFLE_MASK, V8, V8, V8
+		VMOVQ V8, (R5)            // store 12 bytes output
+
+		ADDV $12, R5, R5
+		SUBV $16, R7, R7
+		ADDV $16, R6, R6
+		BGEU R7, R10, loop
+done:
+	MOVV R7, ret+48(FP)
 	RET
 
 //func decodeUrlAsmdst, src []byte) int
