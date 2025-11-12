dc9883d6fa2bbf7097c95ba8321271c49b936982
Sun Yimin <emmansun@users.noreply.github.com>
2025-11-12 13:44:31 +0800

    debug 7

--- base64_loong64.s
+++ base64_loong64.s
@@ -153,7 +153,8 @@ loop:
 		// translate
 		VSEQB V8, DECODE_END, V10 // compare 0x2F with input bytes
 		VADDB V9, V10, V10        // add eq_2F with hi_nibbles
-		VMOVQ V8, (R5)           // store 12 bytes output
+		VSRLB $4, V8, V9      // high nibble
+		VMOVQ V9, (R5)           // store 12 bytes output
 
 		ADDV $12, R5, R5
 		SUBV $16, R7, R7
